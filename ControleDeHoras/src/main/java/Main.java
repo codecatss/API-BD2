09deb0a81c53fb1560031c525dd81effa03b6dff
Willian Danko Caboski <caboskidanko@gmail.com>
2023-03-31 21:50:48 -0300

    Update Main.java

--- ControleDeHoras/src/main/java/Main.java
+++ ControleDeHoras/src/main/java/Main.java
@@ -1,3 +1,6 @@
+
+import javafx.stage.Stage;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,9 @@
  * @author danko
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        TelaBrancaComBotao tela = new TelaBrancaComBotao();
+        Stage stage = new Stage();
+        tela.start(stage);
+    }
 }
