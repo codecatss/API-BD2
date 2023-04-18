5d7ae8b7414fb122192d0e5965956b3a6da316e4
Nicole Souza <nicolemadeleines@gmail.com>
2023-04-18 11:59:01 -0300

    Atualização do CSS e adição das imagens.

--- API-BD2/src/main/java/com/mycompany/api/bd2/App.java
+++ API-BD2/src/main/java/com/mycompany/api/bd2/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Login"), 640, 480);
+        scene = new Scene(loadFXML("GestorAprovacao"), 1300, 800);
         stage.setScene(scene);
         stage.show();
     }
