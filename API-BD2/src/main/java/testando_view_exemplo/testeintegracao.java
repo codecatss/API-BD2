c23116144ca9d7a7267227f424a5ff2a92641d01
Mikaéla Begotti <mikaela.begotti@vocedm.com.br>
2023-04-19 20:38:44 -0300

    alteracao dao

--- API-BD2/src/main/java/testando_view_exemplo/testeintegracao.java
+++ API-BD2/src/main/java/testando_view_exemplo/testeintegracao.java
@@ -4,7 +4,10 @@
 
 package testando_view_exemplo;
 
+import daos.horaDAO;
 import daos.usuarioDAO;
+import java.sql.Date;
+import models.Hora;
 import models.Usuario;
 
 /**
@@ -25,5 +28,13 @@ public class testeintegracao {
       usuario.setStatus("inativo");
       usuarioDao.save(usuario);
       usuarioDao.getContatos();
+      
+     horaDAO horaDao = new horaDAO();
+      Hora hora = new Hora();
+      hora.setUsername_lancador("Estacius VanderGraaf");
+      
+      hora.setData_hora_inicio(01,04,2023,12,30);
+      hora.setData_hora_fim(01,04,2023,13,30);
+      hora.setTipo("Hora extra");
     }
 }
