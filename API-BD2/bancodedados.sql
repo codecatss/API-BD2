c0999bc3857ff49ef35bc9fa5380f8ff4c1c28b8
Larissa Fernanda <111530654+larissa-fernanda@users.noreply.github.com>
2023-04-22 15:45:42 -0300

    Add colunas (hora), corrige nomes (usuario, integrante)
    Adição de colunas na tabela hora e correção do nome das tabelas usuario e integrante

--- API-BD2/bancodedados.sql
+++ API-BD2/bancodedados.sql
@@ -1,166 +1,184 @@
--- Configuração de usuário e senha padrão do banco de dados para conectar ao JDbC
+-- Criação do banco de dados
+
+create database 2rp;
+
+-- Configurando user e password padrão do banco de dados para conectar ao JDbC
+
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';
 
 GRANT SELECT, INSERT, UPDATE ON 2rp.* TO 'admin'@'localhost';
 
-
 FLUSH PRIVILEGES;
 
--- Criação do banco de dados
-create database 2rp;
-
 -- Define o banco de dados 2rp como o padrão
+
 use 2rp;
 
 -- Criação de tabelas
+
 create table
-    usuarios(
-        username VARCHAR(20) not null PRIMARY KEY,
-        nome VARCHAR (80) not null,
-        senha VARCHAR(15) not NULL,
+    usuario(
+        username VARCHAR(20) NOT NULL,
+        nome VARCHAR (80) NOT NULL,
+        senha VARCHAR(15) NOT NULL,
         funcao ENUM(
             'admin',
             'gestor',
             'colaborador'
         ) NOT NULL,
-        status VARCHAR(10) not NULL
+        status_user ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
+        PRIMARY KEY(username)
     );
 
 create table
     centro_resultado(
         nome VARCHAR(30) NOT NULL,
-        status_aprovacao ENUM('ativo', 'inativo') NOT NULL,
-        codigo_cr VARCHAR(10) not NULL PRIMARY KEY,
-        sigla VARCHAR (10) NOT NULL UNIQUE
+        codigo_cr VARCHAR(10) NOT NULL,
+        sigla VARCHAR (10) NOT NULL,
+        status_cr ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
+        PRIMARY KEY(codigo_cr),
+        UNIQUE(sigla)
     );
 
 create table
     cliente (
+        cnpj BIGINT NOT NULL,
         razao_social VARCHAR(70) NOT NULL,
-        status_clientes ENUM('ativo', 'inativo') NOT NULL,
-        cnpj BIGINT PRIMARY KEY NOT NULL
+        status_cliente ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
+        PRIMARY KEY(cnpj)
     );
 
 create table
     hora(
-        id int not null AUTO_INCREMENT PRIMARY KEY,
-        data_hora_inicio DATETIME not NULL,
-        data_hora_fim DATETIME not NULL,
-        tipo VARCHAR(15) NOT NULL,
-        username_lancador VARCHAR(20) not null,
+        id int AUTO_INCREMENT NOT NULL,
         cod_cr VARCHAR(30) NOT NULL,
-        justificativa VARCHAR(500),
-        projeto VARCHAR(100),
-        Foreign Key (username_lancador) REFERENCES usuarios(username),
-        Foreign Key (cod_cr) REFERENCES centro_resultado(codigo_cr)
+        username_lancador VARCHAR(20) NOT NULL,
+        cnpj_cliente BIGINT NOT NULL,
+        data_hora_inicio DATETIME NOT NULL,
+        data_hora_fim DATETIME NOT NULL,
+        tipo VARCHAR(15) NOT NULL,
+        justificativa_lancamento VARCHAR(500) NOT NULL,
+        projeto VARCHAR(100) NOT NULL,
+	username_aprovador VARCHAR(20),
+        justificativa_negacao VARCHAR(500),
+        status_aprovacao ENUM('pendente','aprovado','negado') NOT NULL DEFAULT 'pendente',
+        Foreign Key (username_lancador) REFERENCES usuario(username),
+        Foreign Key (cod_cr) REFERENCES centro_resultado(codigo_cr),
+        Foreign KEY (cnpj_cliente) REFERENCES cliente(cnpj),
+        Foreign Key (username_aprovador) REFERENCES usuario(username),
+        PRIMARY KEY(id)
     );
 
 create table
     contrato(
-		id int(10) auto_increment primary key,
-        cod_cr VARCHAR(10) not NULL,
+        id int(10) AUTO_INCREMENT NOT NULL,
+        cod_cr VARCHAR(10) NOT NULL,
         cnpj_cliente BIGINT NOT NULL,
         Foreign Key (cod_cr) REFERENCES centro_resultado(codigo_cr),
-        Foreign KEY (cnpj_cliente) REFERENCES cliente(cnpj)
+        Foreign KEY (cnpj_cliente) REFERENCES cliente(cnpj),
+        PRIMARY KEY(id)
     );
 
 create table
-    integrantes (
+    integrante(
         gestor BOOLEAN NOT NULL,
-        username_integrante VARCHAR(20) not null,
-        cod_cr VARCHAR(10) not NULL,
-        Foreign Key (username_integrante) REFERENCES usuarios(username),
+        username_integrante VARCHAR(20) NOT NULL,
+        cod_cr VARCHAR(10) NOT NULL,
+        Foreign Key (username_integrante) REFERENCES usuario(username),
         Foreign Key (cod_cr) REFERENCES centro_resultado(codigo_cr),
         PRIMARY KEY (username_integrante, cod_cr)
     );
 
--- Inserção de dados na tabela
+-- Inserção de dados nas tabelas
+
+-- Adição de Usuários
+
 INSERT INTO
-    usuarios(
-        username,
-        nome,
-        senha,
-        funcao,
-        status
+    usuario
+    (
+    username,
+    nome,
+    senha,
+    funcao
     )
-VALUES (
+VALUES
+    (
         'admin',
         'Admin',
         'admin123',
-        'admin',
-        'Ativo'
+        'admin'
     );
 
 INSERT INTO
-    usuarios(
-        username,
-        nome,
-        senha,
-        funcao,
-        status
+    usuario
+    (
+    username,
+    nome,
+    senha,
+    funcao
     )
-VALUES (
+VALUES
+    (
         'brendel',
         'Brendel Marques',
         'dev123',
-        'colaborador',
-        'Ativo'
-    ), (
+        'colaborador'
+    ),
+    (
         'caio',
         'Caio Sousa',
         'dev123',
-        'colaborador',
-        'Ativo'
-    ), (
+        'colaborador'
+    ),
+    (
         'larissa',
         'Larissa Fernanda',
         'dev123',
-        'colaborador',
-        'Ativo'
-    ), (
+        'colaborador'
+    ),
+    (
         'laroy',
         'Laroy Prado',
         'dev123',
-        'colaborador',
-        'Ativo'
-    ), (
+        'colaborador'
+    ),
+    (
         'markus',
         'Markus Gomes',
         'dev123',
-        'colaborador',
-        'Ativo'
-    ), (
+        'colaborador'
+    ),
+    (
         'mikaela',
         'Mikaela Petronilho',
         'dev123',
-        'colaborador',
-        'Ativo'
-    ), (
+        'colaborador'
+    ),
+    (
         'nicole',
         'Nicole Souza',
         'dev123',
-        'gestor',
-        'Ativo'
-    ), (
+        'gestor'
+    ),
+    (
         'willian',
         'Willian Danko',
         'dev123',
-        'colaborador',
-        'Ativo'
+        'colaborador'
     );
 
 -- Adição de centro de resultados
 
 INSERT INTO
-    centro_resultado (
-        nome,
-        status_aprovacao,
-        codigo_cr,
-        sigla
+    centro_resultado
+    (
+    nome,
+    codigo_cr,
+    sigla
     )
-VALUES (
+VALUES
+    (
         'Codecats',
-        'ativo',
         '13652',
         'CCTS'
     );
@@ -168,77 +186,90 @@ VALUES (
 -- Adição de Clientes
 
 INSERT INTO
-    cliente (
-        razao_social,
-        status_clientes,
-        cnpj
+    cliente
+    (
+    razao_social,
+    cnpj
     )
-VALUES ('Fatec', 'ativo', 123456789), 
-('2RP', 'ativo', 987654321);
+VALUES
+    ('Fatec', 123456789),
+    ('2RP', 987654321);
 
--- Adição de integrantes no centro de resultado
+-- Adição integrantes no centro de resultado
 
 INSERT INTO
-    integrantes (
-        gestor,
-        username_integrante,
-        cod_cr
+    integrante
+    (
+    gestor,
+    username_integrante,
+    cod_cr
     )
-VALUES (true, "nicole", 13652),
-(false, "brendel", 13652),
-(false, "caio", 13652),
-(false, "larissa", 13652),
-(false, "laroy", 13652),
-(false, "markus", 13652), 
-(false, "mikaela", 13652),
-(false, "willian", 13652);
+VALUES
+    (true, "nicole", 13652),
+    (false, "brendel", 13652),
+    (false, "caio", 13652),
+    (false, "larissa", 13652),
+    (false, "laroy", 13652),
+    (false, "markus", 13652),
+    (false, "mikaela", 13652),
+    (false, "willian", 13652);
 
--- Adição dados de contrato
+-- Adição dados de contrato cnpj
 
 INSERT INTO
-    contrato (cod_cr, cnpj_cliente)
-VALUES ("13652", 987654321);
+    contrato
+    (cod_cr, cnpj_cliente)
+VALUES
+    ("13652", 987654321);
 
--- Adição de dados à tabela hora
+-- Adição de horas à tabela
 
 INSERT INTO
-    hora(
-        data_hora_inicio,
-        data_hora_fim,
-        tipo,
-        username_lancador,
-        cod_cr,
-        justificativa,
-        projeto
+    hora
+    (
+    cod_cr,
+    username_lancador,
+    cnpj_cliente,
+    data_hora_inicio,
+    data_hora_fim,
+    tipo,
+    justificativa_lancamento,
+    projeto
     )
-VALUES (
-        "2023-04-15 23:30:00",
-        "2023-04-16 01:20:00",
-        "hora-extra",
+VALUES
+    (
+        13652,
         "larissa",
-        13652,
-        "Correção de bugs",
-        "Projeto Integrador"
-    ), (
+        123456789,
         "2023-04-15 23:30:00",
         "2023-04-16 01:20:00",
         "hora-extra",
-        "laroy",
+        "Populando os bancos",
+        "Projeto Integrador"
+    ),
+    (
         13652,
+        "laroy",
+        123456789,
+        "2023-04-15 23:30:00",
+        "2023-04-16 01:20:00",
+        "hora-extra",
+        "Populando os bancos",
+        "Projeto Integrador"
+    ),
+    (
+        13652,
+        "caio",
+        123456789,
+        "2023-04-15 21:30:00",
+        "2023-04-16 23:20:00",
+        "hora-extra",
         "Conexão de banco de dados JDBC",
         "Projeto Integrador"
     );
 
--- Seleção de dados na tabela
+-- Aprovação de hora-extra na tabela hora
 
-SELECT * FROM usuarios;
+UPDATE hora SET username_aprovador = 'nicole', status_aprovacao = 'aprovado' WHERE id = 1;
 
-SELECT * FROM centro_resultado;
-
-SELECT * FROM cliente;
-
-SELECT * FROM integrantes;
-
-SELECT * FROM contrato;
-
-SELECT * FROM hora;
+UPDATE hora SET username_aprovador = 'nicole', justificativa_negacao ='não foi pedido',status_aprovacao = 'negado' WHERE id = 3;
