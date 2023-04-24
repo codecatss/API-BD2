6f11a63b531c9baa5bb0b64fbd6f796e98779273
Larissa Fernanda <111530654+larissa-fernanda@users.noreply.github.com>
2023-04-23 22:58:18 -0300

    Revert "correcao timestamp"

--- API-BD2/src/main/java/com/mycompany/api/bd2/LancamentoColaboradorController.java
+++ API-BD2/src/main/java/com/mycompany/api/bd2/LancamentoColaboradorController.java
@@ -136,8 +136,8 @@ public class LancamentoColaboradorController {
             Hora hora = new Hora();
             hora.setProjeto(stringProjeto.getText());
             hora.setCod_cr("Cr");
-            hora.setData_hora_inicio(timestamp_inicio);
-            hora.setData_hora_fim(timestamp_fim);
+            hora.setData_hora_inicio(timestamp_inicio.toString());
+            hora.setData_hora_fim(timestamp_fim.toString());
             hora.setUsername_lancador("Joazinho");
             hora.setCnpj_cliente(987654321);
             hora.setJustificativa_lancamento("Muita demanda");
