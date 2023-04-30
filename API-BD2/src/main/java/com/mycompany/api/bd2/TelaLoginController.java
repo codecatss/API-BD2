33528bce6cb8ea65b370025f10a54b832e021e0f
larissa-fernanda <laliffr@gmail.com>
2023-04-29 22:56:25 -0300

    Faz o botão fechar funcionar

--- API-BD2/src/main/java/com/mycompany/api/bd2/TelaLoginController.java
+++ API-BD2/src/main/java/com/mycompany/api/bd2/TelaLoginController.java
@@ -10,10 +10,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class TelaLoginController implements Initializable {
 
@@ -72,6 +74,10 @@ public class TelaLoginController implements Initializable {
 
     @FXML
     private void handleFecharButtonAction(ActionEvent event) {
-        // Fechar a tela de login
+        // Obtém a janela atual
+       Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+       // Fecha a janela atual
+       stage.close();
     }
 }
