ff1545daec6df9cccf6c87ab2da41bcf86072d0d
Mikaéla Begotti <mikaela.begotti@vocedm.com.br>
2023-04-30 14:53:29 -0300

    a

--- API-BD2/src/main/java/models/Usuario.java
+++ API-BD2/src/main/java/models/Usuario.java
@@ -11,9 +11,7 @@ package models;
  */
 public class Usuario {
 
-    public static Usuario getInstance() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    
     private String username = "";
     private String nome = "";
     private String senha = "";
