81c8282d6bf50215a1b8e9e9b66c90f31f0aae5a
larissa-fernanda <laliffr@gmail.com>
2023-04-29 22:20:00 -0300

    Altera o if e chama a proxima tela

--- API-BD2/src/main/java/com/mycompany/api/bd2/TelaLoginController.java
+++ API-BD2/src/main/java/com/mycompany/api/bd2/TelaLoginController.java
@@ -12,6 +12,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -40,12 +41,13 @@ public class TelaLoginController implements Initializable {
 
         try (Connection connection = Conexao.createConnectionToMySQL()) {
             Usuario usuario = new usuarioDAO().getUsuario(user, senha);
-            if (usuario.getUsername().equals( user) && usuario.getSenha().equals(senha)) {
+            if (usuario!=null && usuario.getUsername().equals( user) && usuario.getSenha().equals(senha)) {
                 
                 System.out.println("Logado");
                 System.out.println(usuario.getNome());
-
+                LoginSenha.setText("");
                 // Usuário e senha são válidos, exibir próxima tela
+                App.setRoot("LancamentoColaborador");
                 
             } else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
