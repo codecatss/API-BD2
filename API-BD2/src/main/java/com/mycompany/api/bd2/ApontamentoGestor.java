372dd3defed1ac2fb1e2e4a546eebd8d5b2c1aa0
Caio Sousa <Caio.sousa5@fatec.sp.gov.br>
2023-05-06 23:20:40 -0300

    commit intermediario
    necessario salvamento de alterações para puxar novas mudanças na branch developing

--- API-BD2/src/main/java/com/mycompany/api/bd2/ApontamentoGestor.java
+++ API-BD2/src/main/java/com/mycompany/api/bd2/ApontamentoGestor.java
@@ -5,8 +5,10 @@
 package com.mycompany.api.bd2;
 
 import static com.mycompany.api.bd2.TelaLoginController.usuariologado;
+import com.mycompany.api.bd2.models.Hora;
 import com.mycompany.api.bd2.models.Usuario;
 import java.net.URL;
+import java.sql.Timestamp;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -14,6 +16,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 
 /**
@@ -47,13 +50,43 @@ public class ApontamentoGestor implements Initializable {
     private Button botaoAprovar1;
     @FXML
     private Button botaoReprovar1;
+
+    @FXML
+    private TableView<Hora> tabelaApontamento;
+
+    @FXML
+    private TableColumn<Hora, String> colunaCR;
+
+    @FXML
+    private TableColumn<Hora, String> colunaEmpresa;
+
+    @FXML
+    private TableColumn<Hora, String> colunaFunçao;
+
+    @FXML
+    private TableColumn<Hora, String> colunaGestor;
+
+    @FXML
+    private TableColumn<Hora, String> colunaNome;
+
+    @FXML
+    private TableColumn<Hora, String> colunaProjeto;
+
+    @FXML
+    private TableColumn<Hora, String> colunaUsername;
+    @FXML
+    private TableColumn<Hora, Timestamp> colunaFim;
+    @FXML
+    private TableColumn<Hora, Timestamp> colunaInicio;
+
+    
     
     public static Usuario getUsuario1() {
         return usuariologado;
     }
     public static Usuario usuariologado = new Usuario();
     private String usuario = TelaLoginController.usuariologado.getUsername();
-    
+
     /**
      * Initializes the controller class.
      */
@@ -61,9 +94,60 @@ public class ApontamentoGestor implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         nomeUsuario.setText(usuario);
-    }    
-    public void fechaTela(){
-    Platform.exit();
+    }
+
+    public void fechaTela() {
+        Platform.exit();
+    }
+
+    public class ExportaCSV extends App {
+
+        private static final String CSV_FILE = "Relatorio.csv";
+
+    }
+    /*
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        TableView<Person> table = new TableView<>(); // tabela para exibir dados
+        
+        TableColumn<Person, String> firstNameCol = new TableColumn<>("First Name"); // coluna para exibir primeiro nome
+        firstNameCol.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        
+        TableColumn<Person, String> lastNameCol = new TableColumn<>("Last Name"); // coluna para exibir sobrenome
+        lastNameCol.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        
+        TableColumn<Person, Integer> ageCol = new TableColumn<>("Age"); // coluna para exibir idade
+        ageCol.setCellValueFactory(cellData -> cellData.getValue().ageProperty().asObject());
+        
+        table.getColumns().addAll(firstNameCol, lastNameCol, ageCol); // adicionando colunas à tabela
+        
+        ObservableList<Person> data = readCsv(); // lendo dados do arquivo CSV e convertendo em objetos
+        
+        table.setItems(data); // exibindo dados na tabela
+        
+        BorderPane root = new BorderPane(table);
+        Scene scene = new Scene(root, 400, 300);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
+    private ObservableList<Person> readCsv() throws IOException {
+        ObservableList<Person> data = FXCollections.observableArrayList(); // lista para armazenar objetos criados
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(CSV_FILE))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(","); // dividindo linha em campos
+                String firstName = fields[0];
+                String lastName = fields[1];
+                int age = Integer.parseInt(fields[2]);
+                data.add(new Person(firstName, lastName, age)); // adicionando objeto à lista
+            }
+        }
+        
+        return data;
+    }
+     */
 }
