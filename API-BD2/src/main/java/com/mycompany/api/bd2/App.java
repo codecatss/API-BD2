6e3648856feab191a582710e7b94bf6c10ac8969
larissa-fernanda <laliffr@gmail.com>
2023-05-07 13:08:02 -0300

    Configura para abrir o programa do tamanho da tela de login

--- API-BD2/src/main/java/com/mycompany/api/bd2/App.java
+++ API-BD2/src/main/java/com/mycompany/api/bd2/App.java
@@ -1,38 +1,33 @@
 package com.mycompany.api.bd2;
 
-import com.mycompany.api.bd2.daos.usuarioDAO;
-import com.mycompany.api.bd2.models.Usuario;
-import testando_view_exemplo.testeLeitura;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
 public class App extends Application {
-    
+
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CadastroClienteADM.fxml"));
-        Parent root = fxmlLoader.load();
-        scene = new Scene(root, 1300, 650);
-        stage.setScene(scene);
-        stage.show();
-        }
-    
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("TelaLogin.fxml"));
+    Parent root = loader.load();
+    Scene cena = new Scene(root);
+    stage.setScene(cena);
+    stage.show();
+    };
 
     public static void main(String[] args) {
         launch();
     }
-    
+
     public static Scene getScene() {
         return scene;
     }
@@ -42,7 +37,7 @@ public class App extends Application {
         Parent root = fxmlLoader.load();
         scene.setRoot(root);
     }
-    
+
     public static void setScene(Scene newScene) {
         scene = newScene;
     }
