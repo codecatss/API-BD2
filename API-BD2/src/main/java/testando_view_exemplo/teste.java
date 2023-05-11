a8d94be63d371d2886972a8464674877a75a02a6
BrendelMarques <brendel.marques@fatec.sp.gov.br>
2023-05-10 21:32:58 -0300

    criacao update horaDAO

--- API-BD2/src/main/java/testando_view_exemplo/teste.java
+++ API-BD2/src/main/java/testando_view_exemplo/teste.java
@@ -10,22 +10,38 @@ import com.mycompany.api.bd2.daos.usuarioDAO;
 import com.mycompany.api.bd2.models.Centro_resultado;
 import com.mycompany.api.bd2.models.Hora;
 import com.mycompany.api.bd2.models.Usuario;
+import java.text.ParseException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  * @author mikaela.begotti
  */
 public class teste {
-    public static void main(String[] args) {
-        Centro_resultado cr = new Centro_resultado();
-        crDAO crdao = new crDAO();
+    public static void main(String[] args) throws ParseException {
         
-        cr.setCodigo_cr("13652");
-        cr.setNome("Codecats");
-        cr.setSigla("CCTS");
-        cr.setStatus_cr("inativo");
+        Hora hora = new Hora();
+        horaDAO horadao = new horaDAO();
         
-        crdao.update(cr);
+        hora.setCnpj_cliente(123456789);
+        hora.setCod_cr("13652");
+        hora.setData_hora_fim("2023-04-16 01:20:00");
+        hora.setData_hora_inicio("2023-04-15 23:30:00");
+        hora.setJustificativa_lancamento("Populando os bancos");
+        hora.setJustificativa_negacao("");
+        hora.setProjeto("me hours");
+        hora.setSolicitante("Brendel");
+        hora.setStatus_aprovacao("pendente");
+        hora.setTipo("SOBREAVISO");
+        hora.setUsername_aprovador("nicole");
+        hora.setUsername_lancador("brendel");
+        hora.setId(4);
+        
+        
+   
+           
+        horadao.update(hora);
 
 }
     
