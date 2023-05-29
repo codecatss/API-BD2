6e24e425d28b22b9d30502a1fbc9e51128b69da4
Nicole Souza <nicolemadeleines@gmail.com>
2023-05-29 19:56:06 -0300

    Tela de extração de relatório
    Tela e controller de extração de relatório

---
+++ API-BD2/src/main/java/com/mycompany/api/bd2/ExtracaoRelatorioController.java
@@ -0,0 +1,98 @@
+package com.mycompany.api.bd2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+
+public class ExtracaoRelatorioController {
+
+    @FXML
+    private Button BotaoAprovar;
+
+    @FXML
+    private Button botaoSair;
+
+    @FXML
+    private TableColumn<?, ?> colunaCR;
+
+    @FXML
+    private TableColumn<?, ?> colunaEmpresa;
+
+    @FXML
+    private TableColumn<?, ?> colunaFim;
+
+    @FXML
+    private TableColumn<?, ?> colunaFim1;
+
+    @FXML
+    private TableColumn<?, ?> colunaFuncao;
+
+    @FXML
+    private TableColumn<?, ?> colunaInicio;
+
+    @FXML
+    private TableColumn<?, ?> colunaJust;
+
+    @FXML
+    private TableColumn<?, ?> colunaProjeto;
+
+    @FXML
+    private TableColumn<?, ?> colunaUsername;
+
+    @FXML
+    private Button fecharTela;
+
+    @FXML
+    private ImageView imagemUser;
+
+    @FXML
+    private Button menuApontamento;
+
+    @FXML
+    private Button menuLancamento;
+
+    @FXML
+    private Button menuRelatorio;
+
+    @FXML
+    private Button minimizarTela;
+
+    @FXML
+    private Label nomeUsuario;
+
+    @FXML
+    private Label nometelaatual;
+
+    @FXML
+    private TableView<?> tabelaApontamento;
+
+    @FXML
+    void BotaoSair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RelatorioCSV(ActionEvent event) {
+
+    }
+
+    @FXML
+    void botaoAprovar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fechaTela(ActionEvent event) {
+
+    }
+
+    @FXML
+    void navLancamentoColaborador(ActionEvent event) {
+
+    }
+
+}
