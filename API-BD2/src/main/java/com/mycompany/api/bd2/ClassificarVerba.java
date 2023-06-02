5b333a919b104ba72f50f183914d896f6168690d
markusgomes <contato.markus@gmail.com>
2023-06-02 15:47:44 -0300

    Diferença minutos

--- API-BD2/src/main/java/com/mycompany/api/bd2/ClassificarVerba.java
+++ API-BD2/src/main/java/com/mycompany/api/bd2/ClassificarVerba.java
@@ -10,7 +10,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ClassificarVerba {
-      
+    
     
     //VERIFICACÃO DIAS DA SEMANA
     public boolean diaUtil(int diaSelecionado) {
@@ -19,6 +19,44 @@ public class ClassificarVerba {
     
     public boolean diaNaoUtil(int diaSelecionado) {
         return diaSelecionado == Calendar.SATURDAY || diaSelecionado == Calendar.SUNDAY;
+    }
+    
+    
+    //DIFERENÇA EM MINUTOS
+    public long calcularDiferencaPorDia(int horaInicio, int minutoInicio) {
+        Calendar inicio = Calendar.getInstance();
+        inicio.set(Calendar.HOUR_OF_DAY, horaInicio);
+        inicio.set(Calendar.MINUTE, minutoInicio);
+        
+        Calendar diaFimFixo = Calendar.getInstance();
+        diaFimFixo.set(Calendar.HOUR_OF_DAY, 23);
+        diaFimFixo.set(Calendar.MINUTE, 59);
+        
+        Date inicioDate = inicio.getTime();
+        Date fimDate = diaFimFixo.getTime();
+
+        long diferencaMillis = fimDate.getTime() - inicioDate.getTime();
+        long diferencaMinutosDia = TimeUnit.MILLISECONDS.toMinutes(diferencaMillis);
+
+        return diferencaMinutosDia;
+    }
+    
+    public long calcularDiferencaTotal(int horaInicio, int minutoInicio, int horaFim, int minutoFim) {
+        Calendar inicio = Calendar.getInstance();
+        inicio.set(Calendar.HOUR_OF_DAY, horaInicio);
+        inicio.set(Calendar.MINUTE, minutoInicio);
+
+        Calendar fim = Calendar.getInstance();
+        fim.set(Calendar.HOUR_OF_DAY, horaFim);
+        fim.set(Calendar.MINUTE, minutoFim);
+
+        Date inicioDate = inicio.getTime();
+        Date fimDate = fim.getTime();
+
+        long diferencaMillis = fimDate.getTime() - inicioDate.getTime();
+        long diferencaMinutosTotal = TimeUnit.MILLISECONDS.toMinutes(diferencaMillis);
+
+        return diferencaMinutosTotal;
     }
     
     
