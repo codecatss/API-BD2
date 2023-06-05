8299f21539c47940e98c7aca29d55c654af7306a
Willian Danko Caboski <caboskidanko@gmail.com>
2023-06-04 22:10:08 -0300

    Verbas

--- API-BD2/src/main/java/Conexao/Conexao.java
+++ API-BD2/src/main/java/Conexao/Conexao.java
@@ -50,43 +50,48 @@ public class Conexao {
     try (ResultSet rs = stmt.executeQuery(query)) {
         // Cria o escritor do arquivo CSV
         CSVWriter writer = new CSVWriter(new FileWriter(caminhoCompleto));
+        
+        ClassificarVerba classificador = new ClassificarVerba();
 
         // Obtem o metadado do resultado da consulta
         ResultSetMetaData rsmd = rs.getMetaData();
 
         // Obtem o número de colunas
-        int numColunas = rsmd.getColumnCount() + 6;
+        int numColunas = rsmd.getColumnCount();
 
         // Cria o array de Strings com o cabeçalho
-        String[] cabecalho = new String[numColunas];
+        String[] cabecalho = new String[numColunas+6];
         for (int i = 1; i <= numColunas; i++) {
             cabecalho[i - 1] = rsmd.getColumnLabel(i);
+            
+            cabecalho[numColunas] = "1601";
+            cabecalho[numColunas + 1] = "1602";
+            cabecalho[numColunas + 2] = "3000";
+            cabecalho[numColunas + 3] = "3001";
+            cabecalho[numColunas + 4] = "1809";
+            cabecalho[numColunas + 5] = "3016";
+            
         }
-        cabecalho[numColunas - 6] = "1601";
-        cabecalho[numColunas - 5] = "1602";
-        cabecalho[numColunas - 4] = "3000";
-        cabecalho[numColunas - 3] = "3001";
-        cabecalho[numColunas - 2] = "1809";
-        cabecalho[numColunas - 1] = "3016";
-        
         // Escreve o cabeçalho no arquivo CSV
         writer.writeNext(cabecalho);
 
         // Escreve os dados no arquivo CSV
         while (rs.next()) {
-            String[] linha = new String[numColunas];
+            String[] linha = new String[numColunas+6];
             for (int i = 1; i <= numColunas; i++) {
-                linha[i - 1] = rs.getString(i);
-            }
+                if(i<=numColunas){
+                    linha[i - 1] = rs.getString(i);
+                }
+                else{
+                    linha[i - 1] = Long.toString(classificador.verbas.get(1601).getValor());
+                    linha[i - 1] = Long.toString(classificador.verbas.get(1602).getValor());
+                    linha[i - 1] = Long.toString(classificador.verbas.get(3000).getValor());
+                    linha[i - 1] = Long.toString(classificador.verbas.get(3001).getValor());
+                    linha[i - 1] = Long.toString(classificador.verbas.get(1809).getValor());
+                    linha[i - 1] = Long.toString(classificador.verbas.get(3016).getValor());  
             
-            ClassificarVerba classificador = new ClassificarVerba();
-            
-            linha[numColunas - 6] = Long.toString(classificador.verbas.get(1601).getValor());
-            linha[numColunas - 5] = Long.toString(classificador.verbas.get(1602).getValor());
-            linha[numColunas - 4] = Long.toString(classificador.verbas.get(3000).getValor());
-            linha[numColunas - 3] = Long.toString(classificador.verbas.get(3001).getValor());
-            linha[numColunas - 2] = Long.toString(classificador.verbas.get(1809).getValor());
-            linha[numColunas - 1] = Long.toString(classificador.verbas.get(3016).getValor());
+                }
+            }   
             
             writer.writeNext(linha);
         }
